d6433c38aa68fc2a5fb179c280019ff8fe795aa3
DuongTruongThinh <truongthinhduongvo@example.com>
2023-11-28 11:42:18 +0700

    update route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DetailCourse from "./pages/DetailCourse/DetailCourse";
-
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Account from "./pages/Account/Account";
 function App() {
   return (
-    <div>
-      <DetailCourse />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/detail-course" element={<DetailCourse />}></Route>
+        <Route path="/account/:id" element={<Account></Account>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.scss";
+// import "./index.scss";
+import "./styles/main.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
